fc8bc9aa89434c475a8255ddd4938c64a0df4d8d
Fearfyl <wadebuchanan18@gmail.com>
2025-03-29 22:03:31 -0400

    readded server listening functionality

--- Develop/Express.js
+++ Develop/Express.js
@@ -4,3 +4,8 @@ const app = express();
 app.get('/api/hello', (req,res) => {
     res.json({ message: "Hello World"});
 });
+
+const PORT = 3000;
+app.listen (PORT, () => {
+    console.log('Server is running on http://localhost:${PORT}')
+});
